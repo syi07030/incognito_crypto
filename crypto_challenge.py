616d82c7e35cb80c0d14e2ff4c27514b1705537d
HanGeunyoung <hgy@HanGeunyoungui-MacBookPro.local>
2021-08-13 20:44:14 +0900

    writeup

--- crypto_challenge.py
+++ crypto_challenge.py
@@ -1,62 +1,60 @@
 import os
 import binascii
 from Crypto.Cipher import AES
-import hashlib
 
 class XOR:
     def __init__(self):
-        #self.key = os.urandom(5)
+        self.key = os.urandom(5)
+        #print("key = ", binascii.b2a_hex(self.key))
         self.key = binascii.unhexlify("132b37c5f8")
+        #print("length of key = ", len(self.key))
     def encrypt(self, data: bytes) -> bytes:
-        xored = b''
+        enc = b''
         for i in range(len(data)):
-            xored += bytes([data[i] ^ self.key[i % len(self.key)]])
-        return xored
+            enc += bytes([data[i] ^ self.key[i % 5]])
+        return enc
     def decrypt(self, data: bytes) -> bytes:
+        #complete this method
         return self.encrypt(data)
 
 class AES256:
     def __init__(self):
-        #self.key = hashlib.sha256(key).digest
         iv = b'\x00'*16
         self.key = b'12345678901234567890123456789012'
         self.crypto = AES.new(self.key, AES.MODE_CBC, iv)
     def encrypt(self, msg):
         msg = msg + b'\x00'*(32-len(msg))
-        #msg.ljust(32,0)
         cipher = self.crypto.encrypt(msg)
         return cipher
-    def decrypt(self, enc):
+    def decrypt(self, enc): 
+        #complete this method
+        #flag length is 23-byte
         msg = self.crypto.decrypt(enc)
         return msg[:23]
 
 def main():
     pwd = os.getcwd()
-    #flag = open(pwd+'/desktop/hgy/INCOGNITO/ctf/flag.txt', 'r').read().strip()
+    flag = open(pwd+'/desktop/hgy/INCOGNITO/ctf/flag.bin', 'rb').read().strip()
     #print(flag)
-    flag = binascii.unhexlify("494e434f7b643363727970745f7375636365357321217d")
-    print(len(flag)) #23-byte
-    xore = XOR()
-    xore_flag = xore.encrypt(flag)
-    aese = AES256()
-    encrypt_flag = aese.encrypt(xore_flag)
+    #flag = binascii.unhexlify("494e434f7b643363727970745f7375636365357321217d")
+    #print(len(flag)) #23-byte
+    #xore = XOR()
+    #xore_flag = xore.encrypt(flag)
+    #aese = AES256()
+    #encrypt_flag = aese.encrypt(xore_flag)
     
     #fw = open('output.txt', 'w')
     #fw.write("encrypt flag: ", encrypt_flag)
     #fw.close()
-    print(">>>>>encrypt flag: ", encrypt_flag)
+    print(">>>>>encrypt flag: ", flag)
 
     xord = XOR()
     aesd = AES256()
-    decrypt_flag = aesd.decrypt(encrypt_flag)
-    print("only aes decrypt: ", decrypt_flag)
-    print(binascii.b2a_hex(decrypt_flag))
+    decrypt_flag = aesd.decrypt(flag)
+    #print("only aes decrypt: ", decrypt_flag)
+    print("only aes decrypt: ", binascii.b2a_hex(decrypt_flag))
     xord_flag = xord.decrypt(decrypt_flag)
     print ('>>>>>decrypt flag:', xord_flag)
 
 if __name__ == '__main__':
     main()
-
-    #encrypt flag값을 아예 텍스트 파일로 저장해서 문제랑 함께 주고 -> 아니면 그냥 코드에 박아버리기
-    #사람들이 decrypt 부분 함수를 채워서 그 파일을 열어서 복호화를 성공적으로 하면 플래그 값 획득
-    #aes 키 값은 그냥 코드에서 주는 거 확정, xor같은 경우도 그냥 키 값 주어지는 건 어떨지
